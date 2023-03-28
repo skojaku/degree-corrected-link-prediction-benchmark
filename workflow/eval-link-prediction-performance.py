3b64d12e4dd782791582b7c71391c1852fc90f5f
Sadamori Kojaku <skojaku@iu.edu>
2023-03-28 11:24:47 -0400

    add missing py

---
+++ workflow/eval-link-prediction-performance.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2023-03-28 10:34:47
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2023-03-28 10:50:52
+# %%
+import numpy as np
+import pandas as pd
+import sys
+from sklearn.metrics import roc_auc_score
+
+if "snakemake" in sys.modules:
+    input_file = snakemake.input["input_file"]
+    data_name = snakemake.params["data_name"]
+    output_file = snakemake.output["output_file"]
+else:
+    input_file = "../data/"
+    output_file = "../data/"
+
+# ========================
+# Load
+# ========================
+data_table = pd.read_csv(input_file)
+
+# ========================
+# Preprocess
+# ========================
+
+y, ypred = data_table["y"].values, data_table["ypred"].values
+aucroc = roc_auc_score(y, ypred)
+
+# ========================
+# Save
+# ========================
+pd.DataFrame({"score": [aucroc], "data": data_name}).to_csv(output_file, index=False)
---
+++ workflow/network-link-prediction.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2023-01-17 04:25:23
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2023-03-28 10:48:23
+# %%
+from scipy import sparse
+import numpy as np
+import pandas as pd
+import sys
+
+if "snakemake" in sys.modules:
+    input_file = snakemake.input["input_file"]
+    net_file = snakemake.input["net_file"]
+    params = snakemake.params["parameters"]
+    output_file = snakemake.output["output_file"]
+else:
+    input_file = "../data/"
+    output_file = "../data/"
+
+# ========================
+# Load
+# ========================
+edge_table = pd.read_csv(input_file)
+net = sparse.load_npz(net_file)
+model = params["model"]
+
+# ========================
+# Preprocess
+# ========================
+def calc_topo_link_pred_score(r, c, net, predictor):
+
+    # Degree product
+    deg = np.array(net.sum(axis=1)).reshape(-1)
+
+    if predictor == "preferentialAttachment":
+        return deg[r] * deg[c]
+    elif predictor == "commonNeighbors":
+        score = np.array((net[r, :].multiply(net[c, :])).sum(axis=1)).reshape(-1)
+        return score
+    elif predictor == "jaccardIndex":
+        score = np.array((net[r, :].multiply(net[c, :])).sum(axis=1)).reshape(-1)
+        score = score / np.maximum(deg[r] + deg[c] - score, 1)
+        return score
+    elif predictor == "resourceAllocation":
+        deg_inv = 1 / np.maximum(deg, 1)
+        deg_inv[deg == 0] = 0
+        score = np.array(
+            ((net[r, :] @ sparse.diags(deg_inv)).multiply(net[c, :])).sum(axis=1)
+        ).reshape(-1)
+        return score
+    elif predictor == "adamicAdar":
+        log_deg_inv = 1 / np.maximum(np.log(np.maximum(deg, 1)), 1)
+        log_deg_inv[deg == 0] = 0
+        score = np.array(
+            ((net[r, :] @ sparse.diags(log_deg_inv)).multiply(net[c, :])).sum(axis=1)
+        ).reshape(-1)
+        return score
+
+
+src, trg, y = (
+    edge_table["src"].values.astype(int),
+    edge_table["trg"].values.astype(int),
+    edge_table["isPositiveEdge"],
+)
+
+
+ypred = calc_topo_link_pred_score(src, trg, net, model)
+
+# ========================
+# Save
+# ========================
+pd.DataFrame({"y": y, "ypred": ypred}).to_csv(output_file)

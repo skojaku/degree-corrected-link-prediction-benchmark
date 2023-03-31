a50dfbc849f41b107d38d5be4d2845d48dcfe795
Sadamori Kojaku <skojaku@iu.edu>
2023-03-31 17:27:21 -0400

    minor fix

--- workflow/network-link-prediction.py
+++ workflow/network-link-prediction.py
@@ -2,7 +2,7 @@
 # @Author: Sadamori Kojaku
 # @Date:   2023-01-17 04:25:23
 # @Last Modified by:   Sadamori Kojaku
-# @Last Modified time: 2023-03-31 17:25:45
+# @Last Modified time: 2023-03-31 17:27:00
 from scipy import sparse
 import numpy as np
 import pandas as pd
@@ -18,9 +18,6 @@ else:
     input_file = "../data/"
     output_file = "../data/"
 
-# ========================
-# Load
-# ========================
 edge_table = pd.read_csv(input_file)
 net = sparse.load_npz(net_file)
 model = params["model"]

3e4ff6859232f89dc1aa37962662ad600ace8fac
rachithaiyappa <rachithaiyappa96@gmail.com>
2023-04-04 08:51:11 -0400

    brought back original implementation flow

--- workflow/optimal-stacking-generate-cv.py
+++ workflow/optimal-stacking-generate-cv.py
@@ -5,27 +5,25 @@ import pickle as pkl
 from OptimalStackingFunctions import *
 
 if "snakemake" in sys.modules:
-    input_heldout_feature = snakemake.output["input_heldout_feature"]
-    input_train_file = snakemake.input["input_train_net_file"]
-    
-    output_cv_dir = snake
-    output_cv_x_seen_files = snakemake.input["output_cv_x_seen_files"]
-    output_cv_y_seen_files = snakemake.input["output_cv_y_seen_files"]
-    output_cv_x_unseen_files = snakemake.input["output_cv_x_unseen_files"]
-    output_cv_y_unseen_files = snakemake.input["output_cv_y_unseen_files"]
-
-    output_heldout_feature = snakemake.output["output_heldout_feature"]
-    output_train_feature = snakemake.output["output_train_feature"]
+    input_heldout_feature = snakemake.input["input_heldout_feature"]
+    input_train_feature = snakemake.input["input_train_feature"]
+    output_cv_dir = snakemake.output["output_cv_dir"]
+    # output_cv_x_seen_files = snakemake.input["output_cv_x_seen_files"]
+    # output_cv_y_seen_files = snakemake.input["output_cv_y_seen_files"]
+    # output_cv_x_unseen_files = snakemake.input["output_cv_x_unseen_files"]
+    # output_cv_y_unseen_files = snakemake.input["output_cv_y_unseen_files"]
 else:
     input_file = "../data/"
     output_file = "../data/"
 
+with open(input_heldout_feature,"rb") as f:
+    df_ho = pkl.load(f)
+
+with open(input_train_feature,"rb") as f:
+    df_tr = pkl.load(f)
 
 creat_numpy_files(
-    output_cv_x_seen_files,
-    output_cv_y_seen_files,
-    output_cv_x_unseen_files,
-    output_cv_y_unseen_files,
+    output_cv_dir,
     df_ho,
     df_tr,
 )

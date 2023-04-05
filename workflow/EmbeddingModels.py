00044c4a3493a6f276a79c04409891353165b4d1
Munjung Kim <munjkim@iu.edu>
2023-04-04 23:10:28 -0400

    embeddingmodels

--- workflow/EmbeddingModels.py
+++ workflow/EmbeddingModels.py
@@ -23,6 +23,9 @@ def deepwalk(network, dim, window_length=10, num_walks=40):
     return model.transform(dim=dim)
 
 
+##Laplacian Eigen map ==> make a feature 
+## 
+
 @embedding_model
 def leigenmap(network, dim):
     model = embcom.embeddings.LaplacianEigenMap()
@@ -35,6 +38,28 @@ def modspec(network, dim):
     model = embcom.embeddings.ModularitySpectralEmbedding()
     model.fit(network)
     return model.transform(dim=dim)
+
+
+@embedding_model
+def GCN(network,feature_dim,device,dim_h,dim):
+    model = embcom.embeddings.LaplacianEigenMap()
+    model.fit(network)
+    features = model.transform(dim=feature_dim)
+
+    
+    model_GCN, data = embcom.embeddings.GCN(feature_dim,dim_h,dim).to(device), torch.from_numpy(features).to(dtype=torch.float,device = device)
+    model_trained = embcom.train(model_GCN,data,network,device)
+
+    network_c = network.tocoo()
+    
+    edge_list_gcn = torch.from_numpy(np.array([network_c.row, network_c.col])).to(device)
+    
+    
+
+    embeddings = model_trained(data,edge_list_gcn)
+    
+    return embeddings
+
 
 
 # @embedding_model

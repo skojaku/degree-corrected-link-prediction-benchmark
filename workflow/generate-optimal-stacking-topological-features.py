b571a8f471e8548f8bda15a7987f3fc2d33580de
rachithaiyappa <rachithaiyappa96@gmail.com>
2023-04-19 12:24:42 -0400

    edge candidates as input

--- workflow/generate-optimal-stacking-topological-features.py
+++ workflow/generate-optimal-stacking-topological-features.py
@@ -19,6 +19,7 @@ if "snakemake" in sys.modules:
     input_original_edge_table_file = snakemake.input["input_original_edge_table_file"]
     input_heldout_file = snakemake.input["input_heldout_net_file"]
     input_train_file = snakemake.input["input_train_net_file"]
+    input_edge_candidates_file = snakemake.input["input_edge_candidates_file"]
     output_heldout_feature = snakemake.output["output_heldout_feature"]
     output_train_feature = snakemake.output["output_train_feature"]
 else:
@@ -44,36 +45,36 @@ A_tr = A_tr + A_tr.T
 A_tr.data = A_tr.data * 0.0 + 1.0
 A_tr = A_tr.todense()
 
-
+with open(input_edge_candidates_file,"rb") as f:
+    true_false_candidates = pkl.load(f)
+    
 #### extract features #####
 
-# for hyperparmater search (based on the training network (a subgraph of the holdout network))
-A_diff = A_ho - A_tr
-e_diff = sparse.find(sparse.triu(A_diff, 1))  # true candidates
-A_ho_aux = -1 * A_ho + 1
-ne_ho = sparse.find(sparse.triu(A_ho_aux, 1))  # false candidates
-Nsamples = 10000  # number of samples
+# for hyperparmater search using cv and model creation
+#  (based on the training network (a subgraph of the holdout network))
+
+e_diff = true_false_candidates["positives_train"] # true candidates
+ne_ho = true_false_candidates["negatives_train"] # false candidates
+Nsamples = 100  # number of samples
 edge_t_tr, edge_f_tr = get_true_and_false_edges(Nsamples, e_diff, ne_ho)
 # reshape list of tuples to 2D matrix
 edge_t_tr = lot_to_2dmatrix(edge_t_tr)
 edge_f_tr = lot_to_2dmatrix(edge_f_tr)
 # get features
-df_f_tr = gen_topol_feats(A_orig, A_tr, edge_f_tr)
-df_t_tr = gen_topol_feats(A_orig, A_tr, edge_t_tr)
+df_f_tr = gen_topol_feats(Nsamples,A_orig, A_tr, edge_f_tr)
+df_t_tr = gen_topol_feats(Nsamples,A_orig, A_tr, edge_t_tr)
 
-# for training model (based on the holdout network (a subgraph of the actual graph))
-A_diff = A_orig - A_ho
-e_diff = sparse.find(sparse.triu(A_diff, 1))  # true candidates
-A_orig_aux = -1 * A_orig + 1
-ne_orig = sparse.find(sparse.triu(A_orig_aux, 1))  # false candidates
-Nsamples = 10000  # number of samples
+# for testing model (based on the holdout network (a subgraph of the actual graph))
+e_diff = true_false_candidates["positives_test"] # true candidates
+ne_orig = true_false_candidates["negatives_test"] # false candidates
+Nsamples = 100  # number of samples
 edge_t_ho, edge_f_ho = get_true_and_false_edges(Nsamples, e_diff, ne_orig)
 # reshape list of tuples to 2D matrix
 edge_t_ho = lot_to_2dmatrix(edge_t_ho)
 edge_f_ho = lot_to_2dmatrix(edge_f_ho)
 # get features
-df_f_ho = gen_topol_feats(A_orig, A_ho, edge_f_ho)
-df_t_ho = gen_topol_feats(A_orig, A_ho, edge_t_ho)
+df_f_ho = gen_topol_feats(Nsamples,A_orig, A_ho, edge_f_ho)
+df_t_ho = gen_topol_feats(Nsamples,A_orig, A_ho, edge_t_ho)
 
 #### load dataframes for train and holdout sets ####
 df_tr = creat_full_set(df_t_tr, df_f_tr)

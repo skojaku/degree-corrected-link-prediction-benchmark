8ac5d265702abe1879872a78a4f24c8650b8b4a0
Sadamori Kojaku <skojaku@iu.edu>
2023-04-19 21:07:01 -0400

    fix gcn bug

--- workflow/EmbeddingModels.py
+++ workflow/EmbeddingModels.py
@@ -2,7 +2,7 @@
 # @Author: Sadamori Kojaku
 # @Date:   2022-10-14 15:08:01
 # @Last Modified by:   Sadamori Kojaku
-# @Last Modified time: 2023-04-19 18:00:22
+# @Last Modified time: 2023-04-19 20:59:32
 
 import embcom
 import torch
@@ -29,6 +29,13 @@ def calc_prob_i_j(emb, src, trg, net, model_name):
         log_deg = np.log(deg)
         score += log_deg[src] + log_deg[trg]
     return score
+
+
+@embedding_model
+def line(network, dim, num_walks=40):
+    model = embcom.embeddings.Node2Vec(window_length=1, num_walks=num_walks)
+    model.fit(network)
+    return model.transform(dim=dim)
 
 
 @embedding_model
@@ -59,48 +66,48 @@ def modspec(network, dim):
     return model.transform(dim=dim)
 
 
-# @embedding_model
-# def GCN(network,dim,feature_dim=10,device='cpu',dim_h=128):
-#
-#    """
-#    Parameters
-#    ----------
-#    network: adjacency matrix
-#    feature_dim: dimension of features
-#    dim: dimension of embedding vectors
-#    dim_h : dimension of hidden layer
-#    device : device
-#
-#    """
-#
-#    model = embcom.embeddings.LaplacianEigenMap()
-#    model.fit(network)
-#    features = model.transform(dim=feature_dim)
-#
-#
-#    model_GCN, data = embcom.embeddings.GCN(feature_dim,dim_h,dim).to(device), torch.from_numpy(features).to(dtype=torch.float,device = device)
-#    model_trained = embcom.train(model_GCN,data,network,device)
-#
-#    network_c = network.tocoo()
-#
-#    edge_list_gcn = torch.from_numpy(np.array([network_c.row, network_c.col])).to(device)
-#
-#
-#
-#    embeddings = model_trained(data,edge_list_gcn)
-#
-#    return embeddings
+@embedding_model
+def GCN(network, dim, feature_dim=64, device="cpu", dim_h=128):
+    """
+    Parameters
+    ----------
+    network: adjacency matrix
+    feature_dim: dimension of features
+    dim: dimension of embedding vectors
+    dim_h : dimension of hidden layer
+    device : device
 
+    """
 
-# @embedding_model
-# def nonbacktracking(network, dim):
-#    model = embcom.embeddings.NonBacktrackingSpectralEmbedding()
-#    model.fit(network)
-#    return model.transform(dim=dim)
+    model = embcom.embeddings.LaplacianEigenMap()
+    model.fit(network)
+    features = model.transform(dim=feature_dim)
+
+    model_GCN, data = embcom.embeddings.GCN(feature_dim, dim_h, dim).to(
+        device
+    ), torch.from_numpy(features).to(dtype=torch.float, device=device)
+    model_trained = embcom.train(model_GCN, data, network, device)
+
+    network_c = network.tocoo()
+
+    edge_list_gcn = torch.from_numpy(np.array([network_c.row, network_c.col])).to(
+        device
+    )
+
+    embeddings = model_trained(data, edge_list_gcn)
+
+    return embeddings.detach().numpy()
 
 
 @embedding_model
-def graphsage(network, num_walks=1, walk_length=5, dim=10):
+def nonbacktracking(network, dim):
+    model = embcom.embeddings.NonBacktrackingSpectralEmbedding()
+    model.fit(network)
+    return model.transform(dim=dim)
+
+
+@embedding_model
+def graphsage(network, dim, num_walks=1, walk_length=5):
     model = embcom.embeddings.graphSAGE(
         num_walks=num_walks, walk_length=walk_length, emb_dim=dim
     )

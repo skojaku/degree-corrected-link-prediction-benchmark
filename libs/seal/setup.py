2942dbd94f5ab7e3bdf7e3203ba3b50a835c8800
Sadamori Kojaku <skojaku@iu.edu>
2023-06-12 17:23:55 -0400

    add setup.py

---
+++ libs/seal/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Author: Sadamori Kojaku
+# @Date:   2023-06-12 16:04:58
+# @Last Modified by:   Sadamori Kojaku
+# @Last Modified time: 2023-06-12 16:05:08
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name="seal",
+    version="0.0.1",
+    description="embcom",
+    author="Sadamori Kojaku",
+    packages=find_packages(),
+    zip_safe=False,
+)

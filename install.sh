720c6b85685c4ee50f3dbebac673959a50d14399
Sadamori Kojaku <skojaku@bingamton.edu>
2024-04-29 17:48:17 -0400

    update the install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-mamba create -n linkpred -c bioconda -c nvidia -c pytorch -c pyg python=3.11 cuda-version=12.1 pytorch torchvision torchaudio pytorch-cuda=12.1 snakemake graph-tool scikit-learn numpy numba scipy pandas polars networkx seaborn matplotlib gensim ipykernel tqdm black faiss-gpu pyg  -y
-#pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cpu.html
-#pip install torch_geometric
+mamba create -n linkpred -c bioconda -c nvidia -c pytorch -c pyg python=3.11 cuda-version=12.1 pytorch torchvision torchaudio pytorch-cuda=12.1 snakemake graph-tool scikit-learn numpy numba scipy==1.10.1 pandas polars networkx seaborn matplotlib gensim ipykernel tqdm black faiss-gpu pyg python-igraph -y
+pip install adabelief-pytorch==0.2.0
+pip install GPUtil

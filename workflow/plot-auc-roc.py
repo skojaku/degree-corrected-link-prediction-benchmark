0e54a0607c8086ad85bfd1b7799b30e9fc78134e
Sadamori Kojaku <skojaku@bingamton.edu>
2024-05-08 07:00:47 -0400

    rename

--- workflow/plot-auc-roc.py
+++ workflow/plot-auc-roc.py
@@ -1,71 +1,186 @@
-# -*- coding: utf-8 -*-
-# @Author: Sadamori Kojaku
-# @Date:   2023-03-28 10:06:41
-# @Last Modified by:   Sadamori Kojaku
-# @Last Modified time: 2023-05-03 08:49:17
 # %%
-import numpy as np
 import pandas as pd
-import sys
 import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
 
 if "snakemake" in sys.modules:
-    input_file = snakemake.input["input_file"]
+    auc_roc_table_file = snakemake.input["auc_roc_table_file"]
     output_file = snakemake.output["output_file"]
 else:
-    input_file = "../data/derived/results/result_auc_roc.csv"
-    output_file = "../data/opt_stack_auc_roc.png"
+    auc_roc_table_file = "../data/derived/results/result_auc_roc.csv"
+    output_file = "../figs/log_auc_scatter_plot.pdf"
+    output_file_uniform = "../figs/log_auc_scatter_plot_uniform.pdf"
+
+
+def auc_by_model(model, df, ref_data_list=None):
+    """
+    Extracts the AUC scores for a specified model from the dataframe.
+
+    Parameters:
+    - model (str): The name of the model for which AUC scores are to be extracted.
+    - df (DataFrame): The dataframe containing AUC scores along with model names.
+
+    Returns:
+    - A tuple containing:
+        - A numpy array of log-transformed AUC scores for the specified model.
+        - A list of data identifiers corresponding to each score (only if model is 'preferentialAttachment').
+    """
+    AUCs = df.loc[df["model"] == model].sort_values("data")
+    data_list = AUCs["data"].to_list()
+
+    # Check if all data identifiers are the same
+    if ref_data_list is not None:
+        assert all(d == ref_data_list[i] for i, d in enumerate(data_list))
+
+    AUCs = AUCs["log_score"].values
+    if model == "preferentialAttachment":
+        return AUCs, data_list
+    else:
+        return AUCs
+
+
+def cal_auc_ratio(df, n_model):
+
+    df_plot_list = []
+    for sample_id, dg in df.groupby("sampleId"):
+        PA_auc_arr, data_list = auc_by_model("preferentialAttachment", dg)
+
+        diff_arr = np.array(
+            [
+                auc_by_model(model, df, ref_data_list=data_list) - PA_auc_arr
+                for model in model_list
+            ]
+        )
+        diff_arr = diff_arr.reshape(-1)
+        df_plot_list.append(
+            pd.DataFrame(
+                data={
+                    "Score": diff_arr,
+                    "Data": data_list * n_model,
+                    "sample": sample_id,
+                    "model": sum([[m] * len(data_list) for m in model_list], []),
+                }
+            )
+        )
+    df_plot = pd.concat(df_plot_list, ignore_index=True)
+    return df_plot
+
+
+def get_joint_df_plot(df_uniform, df_biased, n_model):
+    df_plot_uniform = cal_auc_ratio(df_uniform, n_model)
+    df_plot_degreeBiased = cal_auc_ratio(df_biased, n_model)
+
+    df_plot_uniform["negativeEdgeSampler"] = ["Uniform"] * df_plot_uniform.shape[0]
+    df_plot_degreeBiased["negativeEdgeSampler"] = [
+        "Biased"
+    ] * df_plot_degreeBiased.shape[0]
+    df_plot = pd.concat([df_plot_uniform, df_plot_degreeBiased], ignore_index=True)
+    df_plot.Data = pd.Categorical(df_plot.Data)
+    df_plot["data_code"] = df_plot.Data.cat.codes
+    return df_plot
+
 
 # ========================
 # Load
 # ========================
-data_table = pd.read_csv(input_file)
-# %% ========================
-# Style
-# ========================
-plot_data = data_table.copy()
-plot_data = plot_data.rename(columns={"negativeEdgeSampler": "Sampling"})
-
-plot_data["Sampling"] = plot_data["Sampling"].map(
-    {"uniform": "Uniform", "degreeBiased": "Pref. Attach."}
+df_result_auc_roc = pd.read_csv(
+    auc_roc_table_file,
 )
 
-palette = {
-    "Pref. Attach.": "red",
-    "Uniform": "#adadad",
-}
-
-if not ("model" in plot_data.columns):
-    plot_data["model"] = "Optimal Stacking"
-
-# %%
 # ========================
+# Preprocessing
+# ========================
+df_result_auc_roc["log_score"] = np.log(df_result_auc_roc["score"].values)
+df_uniform = df_result_auc_roc[df_result_auc_roc["negativeEdgeSampler"] == "uniform"]
+df_degreeBiased = df_result_auc_roc[
+    df_result_auc_roc["negativeEdgeSampler"] == "degreeBiased"
+]
+model_list = list(set(df_uniform["model"]))
+model_list.remove("preferentialAttachment")
+n_model = len(model_list)
+
+plot_data = get_joint_df_plot(df_uniform, df_degreeBiased, n_model)
+
+score_max = plot_data["Score"].values.max()
+score_min = plot_data["Score"].values.min()
+
+# Let's focus on the "learning" methods
+exclude = [
+    "dcGCN",
+    "dcGAT",
+    "dcGraphSAGE",
+    "dcGIN",
+    "jaccardIndex",
+    "commonNeighbors",
+    "resourceAllocation",
+    "localPathIndex",
+    "localRandomWalk",
+]
+plot_data = plot_data[~plot_data["model"].isin(exclude)]
+
+# %% ========================
 # Plot
 # ========================
+# Create a scatter plot
 sns.set_style("white")
-sns.set(font_scale=1)
+sns.set(font_scale=1.2)
 sns.set_style("ticks")
 
-g = sns.catplot(
-    data=plot_data,
-    x="score",
-    y="model",
-    hue="Sampling",
-    col="data",
-    # row="testEdgeFraction",
-    kind="bar",
-    col_wrap=5,
-    palette=palette,
-    sharex=False,
+g = sns.jointplot(
+    data=plot_data.sort_values("negativeEdgeSampler", ascending=False),
+    x="data_code",
+    y="Score",
+    kind="scatter",
+    joint_kws={"s": 23, "alpha": 0.35, "linewidth": 0},
+    hue="negativeEdgeSampler",
+    palette={"Uniform": "grey", "Biased": sns.color_palette("bright")[1]},
 )
-
-g.set_xlabels("AUC-ROC")
-g.set_ylabels("Model")
-
-# ========================
-# Save
-# ========================
-g.fig.savefig(output_file, bbox_inches="tight", dpi=300)
+g.fig.set_figwidth(9)
+g.fig.set_figheight(5)
+g.ax_marg_x.remove()
+g.ax_joint.tick_params(labelbottom=False)
+g.ax_joint.set_ylabel("Logarithmic AUC-ROC relative to\nPreferential Attachment")
+g.ax_joint.set_xlabel("Networks")
+g.ax_joint.axhline(y=0, linewidth=1.5, color="black", linestyle="--")
+g.ax_marg_y.axhline(y=0, linewidth=1.5, color="black", linestyle="--")
+g.ax_joint.set(xlim=(0, 92), ylim=(score_min - 0.1, score_max + 0.1))
+g.ax_joint.legend(frameon=False, loc="lower left", handletextpad=0.1, markerscale=2)
+g.savefig(output_file, bbox_inches="tight", dpi=300)
 
 # %%
+
+
+sns.set_style("white")
+sns.set(font_scale=1.2)
+sns.set_style("ticks")
+
+g = sns.jointplot(
+    data=plot_data.query("negativeEdgeSampler == 'Uniform'"),
+    x="data_code",
+    y="Score",
+    kind="scatter",
+    joint_kws={"s": 23, "alpha": 0.35, "linewidth": 0},
+    hue="negativeEdgeSampler",
+    palette={"Uniform": "grey", "Biased": sns.color_palette()[1]},
+)
+g.fig.set_figwidth(8)
+g.fig.set_figheight(5)
+g.ax_marg_x.remove()
+g.ax_joint.tick_params(labelbottom=False)
+g.ax_joint.set_ylabel("Logarithmic AUC-ROC relative to\nPreferential Attachment")
+g.ax_joint.set_xlabel("Networks")
+g.ax_joint.axhline(y=0, linewidth=1.5, color="black", linestyle="--")
+g.ax_marg_y.axhline(y=0, linewidth=1.5, color="black", linestyle="--")
+
+g.ax_joint.set(xlim=(0, 92), ylim=(score_min - 0.1, score_max + 0.1))
+g.ax_joint.legend(
+    frameon=False, loc="lower left", handletextpad=0.1, markerscale=2
+).remove()
+g.savefig(output_file_uniform, bbox_inches="tight", dpi=300)
+# %%
+
+# plot_data.query("negativeEdgeSampler == 'Biased' and Score <=0").shape[
+#    0
+# ] / plot_data.query("negativeEdgeSampler == 'Biased'").shape[0]
